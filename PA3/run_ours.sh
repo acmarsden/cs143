7b785d43277cdb56013a8de51ccdc3b06112cb0b
jvrsgsty <jvrsgsty@users.noreply.github.com>
2020-05-22 21:15:19 -0700

    Updates scripts and diffs

--- PA3/run_ours.sh
+++ PA3/run_ours.sh
@@ -10,3 +10,10 @@ for test_file in /afs/ir.stanford.edu/class/cs143/examples/*.cl; do
   ./mysemant $test_file &> $OUT_DIR/our_$filename.out
 done
 
+for test_file in *.cl; do
+  echo "Running our semant on $test_file"
+  filename=$(basename -- "$test_file")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+  ./mysemant $test_file &> $OUT_DIR/our_$filename.out
+done
