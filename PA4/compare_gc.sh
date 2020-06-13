f01972ceed4056e55d0a7d439ae7c0c5ef244a30
jvrsgsty <jvrsgsty@users.noreply.github.com>
2020-06-12 20:30:22 -0700

    Updates script

--- PA4/compare_gc.sh
+++ PA4/compare_gc.sh
@@ -6,9 +6,8 @@ filename=$(basename -- "$1")
 extension="${filename##*.}"
 filename="${filename%.*}"
 
-rm -f $filename.s
-rm -f our_$filename.s
 rm -f $OUT_DIR/our_$filename.out
+rm -f $OUT_DIR/their_$filename.out
 
 echo "Executing $OUT_DIR/our_$filename.s on SPIM"
 /afs/ir.stanford.edu/class/cs143/bin/spim $OUT_DIR/our_$filename.s &> $OUT_DIR/our_$filename.out
@@ -16,7 +15,7 @@ echo "Executing $OUT_DIR/their_$filename.s on SPIM"
 /afs/ir.stanford.edu/class/cs143/bin/spim $OUT_DIR/their_$filename.s &> $OUT_DIR/their_$filename.out
 
 echo "============== Number of times we Garbage Collected "
-grep -c "Garbage" our_$filename.out
+grep -c "Garbage" $OUT_DIR/our_$filename.out
 echo "============== Number of times they Garbage Collected "
-grep -c "Garbage" their_$filename.out
+grep -c "Garbage" $OUT_DIR/their_$filename.out
 
